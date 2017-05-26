fa03dcb2a4c1f29e0877f0d027d981a38bce1a34
Sujata Dev <sujatadev97@gmail.com>
2017-05-26 19:50:29 +0530

    https://www.hackerrank.com/challenges/30-data-types

---
+++ src/hack1.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+# https://www.hackerrank.com/challenges/30-data-types
+use strict;
+use warnings;
+
+my $i = 4;
+my $d = 4.0;
+my $s = 'HackerRank ';
+
+my $a=<>;
+my $b=<>;
+my $c=<>;
+
+print $a+$i."\n";
+printf("%.1f\n",($b+$d));
+print $s.$c."\n";
