eb6cb04cd5758086e1a4742523a770bbf6734833
Sujata Dev <sujatadev97@gmail.com>
2017-05-26 22:39:02 +0530

    added spaces

--- src/hack3.pl
+++ src/hack3.pl
@@ -3,18 +3,18 @@
 use strict;
 use warnings;
 
-my $n=<>;
-if($n % 2!=0)
+my $n = <>;
+if($n % 2 != 0)
 {
     print("Weird");
 }
 else
 {
-    if($n>=2 && $n<=5)
+    if($n >= 2 && $n <= 5)
     {
         print("Not Weird");
     }
-    elsif($n>=6 && $n<=20)
+    elsif($n >= 6 && $n <= 20)
     {
         print("Weird");
     }
