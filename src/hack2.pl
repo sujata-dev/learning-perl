9f766df40ad5ca62f1ea0b6f93f3556d73a6b0d1
Sujata Dev <sujatadev97@gmail.com>
2017-05-26 19:51:30 +0530

    https://www.hackerrank.com/challenges/30-operators

---
+++ src/hack2.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+# https://www.hackerrank.com/challenges/30-operators
+use strict;
+use warnings;
+
+my $mealcost=<>;
+my $tipPercent=<>;
+my $taxPercent=<>;
+my $tip=$mealcost * ($tipPercent/100);
+my $tax=$mealcost * ($taxPercent/100);
+printf("The total meal cost is %.0f dollars.\n",($mealcost+$tip+$tax));
