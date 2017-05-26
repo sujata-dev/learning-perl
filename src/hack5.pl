e3aa46bacb50dad4f0afd554ce23a5ad6ecfbe40
Sujata Dev <sujatadev97@gmail.com>
2017-05-26 21:23:19 +0530

    https://www.hackerrank.com/challenges/30-review-loop

---
+++ src/hack5.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+# https://www.hackerrank.com/challenges/30-review-loop
+use strict;
+use warnings;
+
+my $j;
+my $n=<>;
+for(my $i=0;$i<$n;$i++)
+{
+    my $str=<>;
+    my @arr=split("",$str);
+    my $len=@arr;
+    for($j = 0; $j < $len; $j = $j + 2)
+    {
+        print $arr[$j];
+        chomp(@arr);    #remove newline from end of each element in array
+    }
+    print(" ");
+
+    for($j = 1; $j < $len; $j = $j + 2)
+    {
+        print $arr[$j];
+    }
+    print("\n");
+}
