71a1234a7098925da6e6779b94ec6299848d2a12
Sujata Dev <sujatadev97@gmail.com>
2017-05-26 22:59:08 +0530

    added spaces

--- src/Here_Documents.pl
+++ src/Here_Documents.pl
@@ -2,23 +2,22 @@
 use strict;
 use warnings;
 
-my $num=10;
-my $name="tom";
-my $surname=" Blake";
-my $fullname=$name.$surname;
-my $smile=v9786;
-my $line=<<"end of letter";
+my $num = 10;
+my $name = "tom";
+my $surname = " Blake";
+my $fullname = $name.$surname;
+my $line =<< "end of letter";
 \nHello \u$fullname,
     This is Mr. \"X\", from Apartment $num.
-    Meet me \Uasap.
-Thanks. $smile
+    Meet me \Uasap.\E
+Thanks.
 end of letter
 print($line);
 
-$line=<<'end of letter';
+$line =<< 'end of letter';
 \nHello \u$fullname,
     This is Mr. \"X\", from Apartment $num.
-    Meet me \Uasap.
-Thanks. $smile
+    Meet me \Uasap.\E
+Thanks.
 end of letter
 print("\n$line");
