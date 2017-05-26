109ca44786f9790cc9c951441c1730275ff2527c
Sujata Dev <sujatadev97@gmail.com>
2017-05-26 22:52:08 +0530

    added spaces

--- src/hack5.pl
+++ src/hack5.pl
@@ -4,19 +4,18 @@ use strict;
 use warnings;
 
 my $j;
-my $n=<>;
-for(my $i=0;$i<$n;$i++)
+my $n = <>;
+for(my $i = 0; $i < $n; $i++)
 {
-    my $str=<>;
-    my @arr=split("",$str);
-    my $len=@arr;
+    my $str = <>;
+    my @arr = split("", $str);
+    my $len = @arr;
     for($j = 0; $j < $len; $j = $j + 2)
     {
         print $arr[$j];
         chomp(@arr);    #remove newline from end of each element in array
     }
     print(" ");
-
     for($j = 1; $j < $len; $j = $j + 2)
     {
         print $arr[$j];
