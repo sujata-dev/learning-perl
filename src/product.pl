5a529261f8e944f9b6c70473502c2af347ed95f8
Sujata Dev <sujatadev97@gmail.com>
2017-08-08 21:57:56 +0530

    subroutine to accept two numbers as arguments, and return the product

---
+++ src/product.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+# Subroutine to accept two numbers as arguments, and return the product
+use strict;
+use warnings;
+
+sub product
+{
+    return $_[0] * $_[1];
+}
+print product(2, 3);
