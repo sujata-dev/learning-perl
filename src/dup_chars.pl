8c99b4746681e3d6e90cad3fe238a198d6f59406
Sujata Dev <sujatadev97@gmail.com>
2017-08-08 21:55:54 +0530

    to remove duplicate characters from a user-entered string

---
+++ src/dup_chars.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+# To remove duplicate characters from a user-entered string
+use strict;
+use warnings;
+
+chomp(my $str = <>);
+my (%rep, $newstr);
+for(split("", $str))
+{
+    if(exists($rep{$_}))
+    {
+        next;
+    }
+    $rep{$_}++;
+    $newstr = $newstr . "$_";
+}
+print $newstr;
