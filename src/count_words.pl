a95c7d427cc041c88ca60619e0538032a9ec23a3
Sujata Dev <sujatadev97@gmail.com>
2017-08-08 21:56:33 +0530

    To count the number of words in a user-entered paragraph

---
+++ src/count_words.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+# To count the number of words in a user-entered paragraph
+use strict;
+use warnings;
+
+chomp(my $para = <>);
+my $count = 0;
+for(split("", $para))
+{
+    if($_ eq " ")
+    {
+        $count++;
+    }
+}
+print $count+1;
