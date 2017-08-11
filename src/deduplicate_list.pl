0aaf347bd753213e6233acd0a8353a49b83b3716
Sujata Dev <sujatadev97@gmail.com>
2017-08-12 01:43:02 +0530

    formatting done

--- src/deduplicate_list.pl
+++ src/deduplicate_list.pl
@@ -1,12 +1,11 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 # Subroutine to de-duplicate a list
 use strict;
 use warnings;
-
 
 sub dedup
 {
     return keys %{{map { $_ => 1 } @_}};
 }
 
-print dedup(1,2,6,9,2,1,3);
+print dedup(1, 2, 6, 9, 2, 1, 3);
