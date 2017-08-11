e77d24c8b72ef9b6517ee20471f160ba9f30c60f
Sujata Dev <sujatadev97@gmail.com>
2017-08-12 01:40:02 +0530

    for loop changed

--- src/accept_matrix_n_transpose.pl
+++ src/accept_matrix_n_transpose.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 # To accept a 2D matrix from the user, and print its transpose
 use strict;
 use warnings;
@@ -8,17 +8,19 @@ print "Enter row and column: ";
 chomp(my $str = <>);
 my ($row, $col) = (split(" ", $str));
 print "Enter matrix:\n";
-for(my $i = 0; $i < $row; $i++)
+for(0..$row-1)
 {
     chomp($str = <>);
     my @row = split(" ", $str);
     push @matrix, \@row;
 }
 print "\nTransposed Matrix\n";
-for(my $i = 0; $i < $col; $i++)
+for(0..$col-1)
 {
-    for(my $j = 0; $j < $row; $j++)
+    my $i=$_;
+    for(0..$row-1)
     {
+        my $j=$_;
         print $matrix[$j][$i]." ";
     }
     print "\n";
