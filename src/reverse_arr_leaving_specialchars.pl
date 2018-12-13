2baea9e6d18fe74b7c594a41d56306dd35beda85
Sujata Dev <sujatadev97@gmail.com>
2018-12-13 21:03:11 +0530

    reverse an array without affecting special characters

---
+++ src/reverse_arr_leaving_specialchars.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+# reverse an array without affecting special characters
+use strict;
+use warnings;
+
+
+while(chomp(my $str=<>))
+{
+    my @arr=split("", $str);
+    my ($x, $y) = (-1, scalar(@arr));
+
+    for(0..@arr-1)
+    {
+        if($_ < (@arr/2))
+        {
+            $x++;
+            $y--;
+
+            if($x < $y)
+            {
+                $x++ while((!($arr[$x] =~ /[a-zA-Z]/)) && ($x < $y));
+
+                $y-- while((!($arr[$y] =~ /[a-zA-Z]/)) && ($x < $y));
+
+                ($arr[$x], $arr[$y]) = ($arr[$y], $arr[$x]);
+            }
+        }
+    }
+    print(join("", @arr));
+    print("\n");
+}
