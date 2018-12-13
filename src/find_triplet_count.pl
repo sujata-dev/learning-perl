03a2ea0581c0cfff490ec5b8f703259e6e98791b
Sujata Dev <sujatadev97@gmail.com>
2018-12-13 22:23:49 +0530

    corrected spacing

--- src/find_triplet_count.pl
+++ src/find_triplet_count.pl
@@ -4,19 +4,19 @@ use strict;
 use warnings;
 
 
-chomp(my $arr=<>);
-chomp(my $sum=<>);
+chomp(my $arr = <>);
+chomp(my $sum = <>);
 
-my @arr=sort{$a <=> $b} split(" ", $arr);
-my $count=0;
+my @arr = sort{$a <=> $b} split(" ", $arr);
+my $count = 0;
 
-for my $x (0..@arr-1)
+for my $x (0..@arr - 1)
 {
-    for my $y (($x+1)..@arr-$x)
+    for my $y (($x + 1)..@arr - $x)
     {
-        for my $z (($y+1)..@arr-$y)
+        for my $z (($y + 1)..@arr - $y)
         {
-            $count++ if(($arr[$x]+$arr[$y]+$arr[$z])<$sum);
+            $count++ if(($arr[$x] + $arr[$y] + $arr[$z]) < $sum);
         }
     }
 }
--- src/reverse_arr_leaving_specialchars.pl
+++ src/reverse_arr_leaving_specialchars.pl
@@ -4,28 +4,25 @@ use strict;
 use warnings;
 
 
-while(chomp(my $str=<>))
+chomp(my $str = <>);
+my @arr = split("", $str);
+my ($x, $y) = (-1, scalar(@arr));
+
+for(0..@arr - 1)
 {
-    my @arr=split("", $str);
-    my ($x, $y) = (-1, scalar(@arr));
-
-    for(0..@arr-1)
+    if($_ < (@arr / 2))
     {
-        if($_ < (@arr/2))
+        $x++;
+        $y--;
+
+        if($x < $y)
         {
-            $x++;
-            $y--;
+            $x++ while((!($arr[$x] =~ /[a-zA-Z]/)) && ($x < $y));
 
-            if($x < $y)
-            {
-                $x++ while((!($arr[$x] =~ /[a-zA-Z]/)) && ($x < $y));
+            $y-- while((!($arr[$y] =~ /[a-zA-Z]/)) && ($x < $y));
 
-                $y-- while((!($arr[$y] =~ /[a-zA-Z]/)) && ($x < $y));
-
-                ($arr[$x], $arr[$y]) = ($arr[$y], $arr[$x]);
-            }
+            ($arr[$x], $arr[$y]) = ($arr[$y], $arr[$x]);
         }
     }
-    print(join("", @arr));
-    print("\n");
 }
+print(join("", @arr));
